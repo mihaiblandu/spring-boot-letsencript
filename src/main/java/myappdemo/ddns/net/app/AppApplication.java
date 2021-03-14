ca6015b7ed5138ab6307a61eeb1ee0063bc9b852
mihaiblandu <mihaiblandu@icloud.com>
2021-03-14 21:27:56 +0200

    pdf

--- src/main/java/myappdemo/ddns/net/app/AppApplication.java
+++ src/main/java/myappdemo/ddns/net/app/AppApplication.java
@@ -2,16 +2,20 @@ package myappdemo.ddns.net.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @SpringBootApplication
-@RestController
+@Controller
 public class AppApplication {
 	@GetMapping("")
-	public String hello(){
-		return "Hello in Https";
+	public String hello( ){
+		return "index";
 	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
