465c4728287bd4a9c4f1b3ddc49d8796f980f1a6
mihai blandu <mihaiblandu@icloud.com>
2021-03-14 16:58:55 +0000

    ok

--- installcertificate.sh
+++ installcertificate.sh
@@ -47,4 +47,4 @@ echo $DEST
 echo $ALIAS
 # Add the staging option (--staging) to certbot-auto if you wish to validate the procedure
 certbot certonly --debug --non-interactive --email ${EMAIL} --agree-tos --standalone -d ${DOMAIN} --keep-until-expiring
-openssl pkcs12 -export -in /etc/letsencrypt/live/${DOMAIN}/cert.pem -inkey /etc/letsencrypt/live/${DOMAIN}/privkey.pem -out ${DEST} -name ${ALIAS} -CAfile /etc/letsencrypt/live/${DOMAIN}/chain.pem -caname root -passout pass:${PASSWORD}
+openssl pkcs12 -export -in /etc/letsencrypt/live/${DOMAIN}/cert.pem -inkey /etc/letsencrypt/live/${DOMAIN}*/privkey.pem -out ${DEST} -name ${ALIAS} -CAfile /etc/letsencrypt/live/${DOMAIN}*/chain.pem -caname root -passout pass:${PASSWORD}
