6215dfea92550ff62c0306fe4405adfd888fa840
mihaiblandu <mihaiblandu@icloud.com>
2021-03-14 18:17:57 +0200

    fix_bugs

--- installcertificate.sh
+++ installcertificate.sh
@@ -3,6 +3,7 @@ DOMAIN=$1
 EMAIL=$2
 PASSWORD=$3
 DEST=$4
+ALIAS=$5
 
 if [ -z $DEST ]
 then
@@ -31,10 +32,19 @@ then
 else
       echo ${DEST} "is NOT empty"
 fi
+
+if [ -z $ALIAS ]
+then
+    read -p "Enter ALIAS: " ALIAS
+else
+      echo ${ALIAS} "is NOT empty"
+fi
+
 echo $DOMAIN
 echo $EMAIL
 echo $PASSWORD
 echo $DEST
+echo $ALIAS
 # Add the staging option (--staging) to certbot-auto if you wish to validate the procedure
 certbot certonly --debug --non-interactive --email ${EMAIL} --agree-tos --standalone -d ${DOMAIN} --keep-until-expiring
-openssl pkcs12 -export -in /etc/letsencrypt/live/${DOMAIN}/cert.pem -inkey /etc/letsencrypt/live/${DOMAIN}/privkey.pem -out ${DEST} -name tomcat -CAfile /etc/letsencrypt/live/${DOMAIN}/chain.pem -caname root -passout pass:${PASSWORD}
+openssl pkcs12 -export -in /etc/letsencrypt/live/${DOMAIN}/cert.pem -inkey /etc/letsencrypt/live/${DOMAIN}/privkey.pem -out ${DEST} -name ${ALIAS} -CAfile /etc/letsencrypt/live/${DOMAIN}/chain.pem -caname root -passout pass:${PASSWORD}
